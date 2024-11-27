9fab5f13ed16dcfc8b5031f0ac523407a33de6ba
Jason Lo <lcmjlo@gmail.com>
2024-11-26 18:21:36 -0600

    manual test

--- clean_database/dbruns/populate.sh
+++ clean_database/dbruns/populate.sh
@@ -6,18 +6,22 @@ PGUSER=postgres
 PGHOST=0.0.0.0
 PGPORT=5432
 
-mkdir -p dbout
-if [ ! -f /tmp/pg_data/clean_dump.tar.gz ]; then
-    wget https://neotoma-remote-store.s3.us-east-2.amazonaws.com/clean_dump.tar.gz --no-check-certificate -P /tmp/pg_data/
-fi
+# mkdir -p dbout
+# if [ ! -f /tmp/pg_data/clean_dump.tar.gz ]; then
+#     wget https://neotoma-remote-store.s3.us-east-2.amazonaws.com/clean_dump.tar.gz --no-check-certificate -P /tmp/pg_data/
+# fi
+mkdir -p /pg_data
+tar -xf /data/clean_dump.tar.gz -C /pg_data/
 
-tar -xf /tmp/pg_data/clean_dump.tar.gz -C /tmp/pg_data/
 
 psql -v ON_ERROR_STOP=1 --username "$PGUSER" --dbname postgres <<-EOSQL
-CREATE DATABASE IF NOT EXISTS neotoma;
+DROP DATABASE IF EXISTS neotoma;
+CREATE DATABASE neotoma;
+\c neotoma
 CREATE EXTENSION postgis;
 CREATE EXTENSION pg_trgm;
-
 EOSQL
 
-pg_restore -U $PGUSER -h $PGHOST -p $PGPORT -c --no-owner --no-privileges -d $DATABASE /tmp/pg_data/neotoma_clean.sql
+psql -U $PGUSER -h $PGHOST -p $PGPORT -d $DATABASE -f /pg_data/archives/neotoma_clean.sql
+
+
